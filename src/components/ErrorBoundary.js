f2552544c347a5879b18e2e441c63da0a74c7fc8
Haylee Adamson <17357481+hailstormCodes@users.noreply.github.com>
2021-04-11 22:10:51 -0400

    Added the URL to Github page

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+
+function ErrorBoundary() {
+    //declare new state variable called "hasError, set initial value to false"
+    const [hasError, errorInfo] = useState(false);
+
+    return ( 
+       if {hasError}
+       
+       <h1>Something went wrong! I'm Sorry!</h1>
+    )
+}
+
+
+
 
 class ErrorBoundary extends Component {
     constructor(props) {
-        super(props);
-        this.state = {
-            hasError: false
+      super(props);
+      this.state = { 
+          hasError: false };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        throw new Error('Noooooooo!');
+    }
+
+    render() {
+        if(this.state.hasError) {
+            return <h1>Something went wrong! I'm sorry!</h1>;
         }
-    }
 
-componentDidCatch(error, info) {
-    this.setState({ hasError: true})
-}
-
-render() {
-    if (this.state.hasError) {
-        return <h1>Oooooops! That is not good.</h1>
+        return this.props.children;
     }
-    return this.props.children
-    }
-}
+};
 
 export default ErrorBoundary;
